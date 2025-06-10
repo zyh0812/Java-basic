9c9b73bf69813f87e8194d8d915df1d568f5f7cb
zyh0812 <1635675746@qq.com>
2025-06-10 22:39:15 +0800

    6-10

--- Java-basic/string-moudle/src/com/zyh/test/StrTrain1.java
+++ Java-basic/string-moudle/src/com/zyh/test/StrTrain1.java
@@ -1,18 +1,25 @@
 package com.zyh.test;
 
-public class StrTrain {
+public class StrTrain1 {
     public static void main(String[] args) {
         String strA = "abcde";
-        String strB = "cdeab";
+        String strB = "abcde";
 
         System.out.println(flag(strA,strB));
 
     }
-
     public static String changeStr(String strA){
-        char strStar = strA.charAt(0);
-        String strEnd= strA.substring(1);
-        String newStr = strEnd + strStar;
+        char[] c = strA.toCharArray();
+        char first = c[0];
+
+        for (int i = 1; i < strA.length(); i++) {
+            c[i-1] = c[i];
+        }
+        c[strA.length()-1] = first;
+        String newStr = "";
+        for (int i = 0; i < strA.length(); i++) {
+            newStr = newStr + c[i];
+        }
         return newStr;
     }
 
